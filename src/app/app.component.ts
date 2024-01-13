9bc57f43ca5b463d81615e32cd972b96aee12a93
rhetice.yenou <rhetice.yenou@chem.loc>
2024-01-13 13:04:38 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent],
+  template: `
+      <main>
+        <header>
+          <img class="brand-name" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+        <section class="content">
+          <app-home></app-home>
+        </section>
+      </main>
+      `,
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'TutoAppart';
+  title = 'homes';
 }
